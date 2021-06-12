a89713e933716f5b2648df467fec0d7ab7e60475
NaufalRafif <77334971+NRAF13@users.noreply.github.com>
2021-06-12 17:57:22 +0700

    Add files via upload

--- bot.js
+++ bot.js
@@ -2,9 +2,10 @@ const { Client, MessageEmbed, Channel, Message } = require('discord.js');
 const client = new Client;
 global.config = require("./config.json")
 const query = require("samp-query");
+const getServerPing = require('./src/samp.js')
 const prefix = "!";
 let Samp_IP = "18.141.24.112";
-let Samp_Port = 13210;
+let Samp_Port = 7777;
 var channelid = '837571530904043601';
 
 var options = {
@@ -15,43 +16,56 @@ var s;
 
 client.on('ready', () => {
     console.log(`Bot ${client.user.tag} is going online!!`);
-    UpdateStatus();setInterval(UpdateStatus,1000);
-    query(options, function(error, response){
-        if(error)
-        {
-            status = "0 Players";
-            count = 0;
-            client.user.setActivity(status, {type: 'WATCHING'});
-        }
-        else
-        {
-            client.user.setActivity("0 Players", {type: 'WATCHING'});
-        }
-    })
-    const interval = setInterval(function setStatus(){
-        UpdateStatus();
-        return setStatus;
-    }, 30000)
+    UpdateStatus();setInterval(UpdateStatus,30000)
 });
 
 function UpdateStatus()
 {
+    const randpesan = [
+        "DEWATA ROLEPLAY",
+        "#DEWATARP",
+        "Join us @dewatarp.xyz"
+    ];
+    const randommes = Math.floor(Math.random() * (randpesan.length));
+    const newRandomMes = randpesan[randommes];
+    const randstatus = [
+        "PLAYING",
+        "STREAMING",
+        "LISTENING",
+        "COMPETING",
+        "WATCHING"
+    ]
+    const randomstats = Math.floor(Math.random() * (randstatus.length));
+    const newRandomStats = randstatus[randomstats]
     query(options, function(error, response){
         if(error)
         {
-            status = "0 Players";
-            client.user.setActivity(status, {type: 'PLAYING'});
+            status = "SERVER OFFLINE";
+            client.user.setActivity(status, {type: `${newRandomStats}`});
         }
         else
         {
-            status = `${response['online']} Players`;
-            client.user.setActivity(status, {type: 'PLAYING'});
+            status = `${response['online']} Players | ${newRandomMes}`;
+            client.user.setActivity(status, {type: `${newRandomStats}`});
         }
     })
 }
 
 function getServerInfo(msg)
 {
+    getServerPing("s1.dewatarp.xyz", 7777, function(error,response){
+        if(!error)
+        {
+            spg = `${response}ms`
+        }
+    })
+    const randpesan = [
+        "**```DEWATA ROLEPLAY```**",
+        "**```#DEWATARP```**",
+        "**```Join us @dewatarp.xyz```**"
+    ];
+    const randommes = Math.floor(Math.random() * (randpesan.length));
+    const newRandomMes = randpesan[randommes];
     query(options, function(error, response){
         if(error)
         {
@@ -63,13 +77,15 @@ function getServerInfo(msg)
                     color: embedColor,
                     url: 'https://dewatarp.xyz/',
                     fields: [
-                        { name: 'Server Name', value: 'Dewata Roleplay', inline: false },
-                        { name: 'Website', value: 'https://dewatarp.xyz/', inline: false },
-                        { name: 'Ip', value: 'dewatarp.xyz', inline: false },
-                        { name: 'Gamemode', value: 'DRP v2.1.3b', inline: false },
-                        { name: 'Language', value: 'Bahasa Indonesia', inline: false },
-                        { name: 'Status', value: '🔴Offline', inline: false },
-                        { name: 'Players', value: '0/100', inline: false },
+                        { name: '**Server Name**', value: 'Dewata Roleplay', inline: false },
+                        { name: '**Website**', value: 'https://dewatarp.xyz/', inline: false },
+                        { name: '**IP**', value: 'dewatarp.xyz', inline: false },
+                        { name: '**Gamemode**', value: 'DRP v2.1.3b', inline: false },
+                        { name: '**Language**', value: 'Bahasa Indonesia', inline: false },
+                        { name: '**Status**', value: '🔴Offline', inline: false },
+                        { name: '**Players**', value: '0/100', inline: false },
+                        { name: '**Ping**', value: '0ms', inline: false },
+                        { name: '**Pesan**', value: `${newRandomMes}`, inline: false },
                     ],
                     thumbnail: {
                         url: 'https://dewatarp.xyz/DEWATA.png',
@@ -92,13 +108,15 @@ function getServerInfo(msg)
                     color: embedColor,
                     url: 'https://dewatarp.xyz/',
                     fields: [
-                        { name: 'Server Name', value: response['hostname'], inline: false },
-                        { name: 'Website', value: 'https://dewatarp.xyz/', inline: false },
-                        { name: 'Ip', value: 'dewatarp.xyz', inline: false },
-                        { name: 'Gamemode', value: response['gamemode'], inline: false },
-                        { name: 'Language', value: response['mapname'], inline: false },
-                        { name: 'Status', value: ':green_circle:Online', inline: false },
-                        { name: 'Players', value: `${response['online']}/${response['maxplayers']}`, inline: false },
+                        { name: '**Server Name**', value: response['hostname'], inline: false },
+                        { name: '**Website**', value: 'https://dewatarp.xyz/', inline: false },
+                        { name: '**IP**', value: 'dewatarp.xyz', inline: false },
+                        { name: '**Gamemode**', value: response['gamemode'], inline: false },
+                        { name: '**Language**', value: response['mapname'], inline: false },
+                        { name: '**Status**', value: ':green_circle:Online', inline: false },
+                        { name: '**Players**', value: `${response['online']}/${response['maxplayers']}`, inline: false },
+                        { name: '**Ping**', value: `${spg}`, inline: false },
+                        { name: '**Pesan**', value: `${newRandomMes}`, inline: false },
                     ],
                     thumbnail: {
                         url: 'https://dewatarp.xyz/DEWATA.png',
@@ -113,6 +131,77 @@ function getServerInfo(msg)
         }
     })
 }
+function ServerStatus(msg)
+{
+    getServerPing("s1.dewatarp.xyz", 7777, function(error,response){
+        if(!error)
+        {
+            spg = `${response}ms`
+        }
+    })
+    query(options, function(error, response){
+        if(error)
+        {
+            const embedColor = 0xff0000;
+
+            const logMessage = {
+                embed: {
+                    title: 'DEWATA ROLEPLAY',
+                    color: embedColor,
+                    url: 'https://dewatarp.xyz/',
+                    fields: [
+                        { name: '**Server Name**', value: 'Dewata Roleplay', inline: false },
+                        { name: '**Website**', value: 'https://dewatarp.xyz/', inline: false },
+                        { name: '**IP**', value: 'dewatarp.xyz', inline: false },
+                        { name: '**Gamemode**', value: 'DRP v2.1.3b', inline: false },
+                        { name: '**Language**', value: 'Bahasa Indonesia', inline: false },
+                        { name: '**Status**', value: '🔴Offline', inline: false },
+                        { name: '**Players**', value: '0/100', inline: false },
+                        { name: '**Ping**', value: '0ms', inline: false },
+                    ],
+                    thumbnail: {
+                        url: 'https://dewatarp.xyz/DEWATA.png',
+                    },
+                    timestamp: new Date(),
+                    footer: {
+                        text: 'DEWATA ROLEPLAY',
+                    }
+                }
+            }
+            msg.channel.send(logMessage);
+        }
+        else
+        {
+            const embedColor = 0x800080;
+
+            const logMessage = {
+                embed: {
+                    title: 'DEWATA ROLEPLAY',
+                    color: embedColor,
+                    url: 'https://dewatarp.xyz/',
+                    fields: [
+                        { name: '**Server Name**', value: response['hostname'], inline: false },
+                        { name: '**Website**', value: 'https://dewatarp.xyz/', inline: false },
+                        { name: '**IP**', value: 'dewatarp.xyz', inline: false },
+                        { name: '**Gamemode**', value: response['gamemode'], inline: false },
+                        { name: '**Language**', value: response['mapname'], inline: false },
+                        { name: '**Status**', value: ':green_circle:Online', inline: false },
+                        { name: '**Players**', value: `${response['online']}/${response['maxplayers']}`, inline: false },
+                        { name: '**Ping**', value: `${spg}`, inline: false },
+                    ],
+                    thumbnail: {
+                        url: 'https://dewatarp.xyz/DEWATA.png',
+                    },
+                    timestamp: new Date(),
+                    footer: {
+                        text: 'DEWATA ROLEPLAY',
+                    }
+                }
+            }
+            msg.channel.send(logMessage);
+        }
+    })
+}
 function helpinfo(msg)
 {
     const embedColor = 0x800080;
@@ -122,13 +211,13 @@ function helpinfo(msg)
             title: 'List Command',
             color: embedColor,
             fields: [
-                { name: `\`\`\`${prefix}help\`\`\``, value: 'list cmd', inline: false },
-                { name: `\`\`\`${prefix}info\`\`\``, value: 'get server info', inline: false },
-                { name: `\`\`\`${prefix}players\`\`\``, value: 'get players online', inline: false },
-                { name: `\`\`\`${prefix}start\`\`\``, value: 'starting a live stats (beta)', inline: false },
-                { name: `\`\`\`${prefix}takerole [IC NAME]\`\`\``, value: 'Taking some role on guild', inline: false },
-                { name: `\`\`\`${prefix}stop\`\`\``, value: 'stop live stats', inline: false },
-                { name: `\`\`\`${prefix}ping\`\`\``, value: 'getting ping', inline: false },
+                { name: `**\`\`\`${prefix}help\`\`\`**`, value: '**list cmd**', inline: false },
+                { name: `**\`\`\`${prefix}status\`\`\`**`, value: '**get server status**', inline: false },
+                { name: `**\`\`\`${prefix}players\`\`\`**`, value: '**get players online**', inline: false },
+                { name: `**\`\`\`${prefix}start\`\`\`**`, value: '**starting a live stats**', inline: false },
+                { name: `**\`\`\`${prefix}takerole [IC NAME]\`\`\`**`, value: '**Taking some role on guild**', inline: false },
+                { name: `**\`\`\`${prefix}stop\`\`\`**`, value: '**stop live stats**', inline: false },
+                { name: `**\`\`\`${prefix}ping\`\`\`**`, value: '**getting ping**', inline: false },
             ],
             thumbnail: {
                 url: 'https://dewatarp.xyz/DEWATA.png',
@@ -177,13 +266,8 @@ client.on('message', msg => {
                     }, 1000)
                 })
                 break;
-            case "info":
-                msg.channel.send(`Getting server info...`)
-                .then(msg => {
-                    setTimeout(function() {
-                        getServerInfo(msg)
-                    }, 1000)
-                })
+            case "status":
+                ServerStatus(msg);
                 break;
             case "start":
                 if(msg.member.roles.cache.has('805471217565433927'))
@@ -204,8 +288,15 @@ client.on('message', msg => {
             case "stop":
                 if(msg.member.roles.cache.has('805471217565433927'))
                 {
-                    msg.react('👍')
-                    clearInterval(s)
+                    if(!s) 
+                    {
+                        msg.reply("I Can't find any live Statistics running")
+                    }
+                    else
+                    {
+                        msg.react('👍')
+                        clearInterval(s)
+                    }
                 }
                 else
                 {
@@ -214,14 +305,14 @@ client.on('message', msg => {
                 break;
             case "takerole"://805473200422649876
                 if(msg.channel.id != '813750075736981534') return
-                if(msg.member.roles.cache.has('805473200422649876')) return
+                if(msg.member.roles.cache.find(rs => rs.id == '805473200422649876')) return
                 if(msg.channel.type == "dm") return msg.channel.send("You can't use that on dm!!")
                 let role = msg.guild.roles.cache.find(r => r.id == "805473200422649876");
                 let member = msg.mentions.members.first();
                 if(!parameters.length) return msg.channel.send("Please input your rp name")
                 const nick = "[WARGA] " + parameters.join(" ")
                 if(nick.length > 32) return msg.channel.send("Your name is to long")
-                try {
+                /*try {
                     msg.member.roles.add(role);
                     msg.channel.send("**YOU GAIN ROLE <@&805473200422649876>, AND WELCOME TO DEWATA ROLEPLAY**")
                 } catch{
@@ -234,16 +325,27 @@ client.on('message', msg => {
                 catch{
                     msg.reply("I Can't change the nickname for this user");
                     console.log(Error)
-                };
-                //msg.member.roles.add(role).catch(msg.reply("I Can't Add roles for this user"))
-                //msg.member.setNickname(nick).catch(msg.reply("I Can't change the nickname for this user"))
+                };*/
+                msg.member.roles.add(role).catch(console.log("Error Addng role"))
+                msg.member.setNickname(nick).catch(console.log("Error cange nick"))
                 break;
             case "ping":
+                var svping;
+                getServerPing("s1.dewatarp.xyz", 7777, function(error,response){
+                    if(error)
+                    {
+                        svping = `Server Ping: 0ms`
+                    }
+                    else
+                    {
+                        svping = `Server Ping: ${response}ms`
+                    }
+                })
                 msg.channel.send("Calculating ping...")
                 .then(message => {
                     setTimeout(function(){
                         var ping = message.createdTimestamp - msg.createdTimestamp 
-                        message.edit(`${ping}ms`)
+                        message.edit(`Latency: ${ping}ms\n${svping}`)
                     }, 1000)
                 })
                 break;
---
+++ src/samp.js
@@ -0,0 +1,271 @@
+const dgram = require('dgram');
+const dns = require('dns');
+
+var safeRequest = function(ipaddress, port, opcode, safeResponse) {
+	if(validateIPAddress(ipaddress))
+	{
+		request(ipaddress, port, opcode, function(error, response){safeResponse.apply(ipaddress, [error, response])});
+	}
+	else 
+	{
+		dns.lookup(ipaddress, function(err, result) {
+			if(err) safeResponse.apply(ipaddress, [err, result]);
+			request(ipaddress, port, opcode, function(error, response){safeResponse.apply(ipaddress, [error, response])});
+		})
+	}
+}
+
+var request = function(ipaddress, port, opcode, response) { 
+	var socket = dgram.createSocket("udp4");
+	var packet = Buffer.alloc(10 + opcode.length);
+
+	packet.write('SAMP');
+	packet[4] = ipaddress.split('.')[0];
+	packet[5] = ipaddress.split('.')[1];
+	packet[6] = ipaddress.split('.')[2];
+	packet[7] = ipaddress.split('.')[3];
+	packet[8] = port & 0xFF;
+	packet[9] = port >> 8 & 0xFF;
+	packet[10] = opcode.charCodeAt(0);
+
+	try {
+		socket.send(packet, 0, packet.length, port, ipaddress, function(error, bytes) {
+			if(error) {
+				console.log(error);
+				return -1;
+			}
+		});
+	}
+	catch(error) {console.log(error); return -1;}
+
+
+	var controller = setTimeout(function() {
+		socket.close();
+		return response.apply(ipaddress, [true, "[error] host unavailable - " + ipaddress + ":" + port]);
+	}, 2000);
+
+	socket.on('message', function (message) 
+	{
+		if(controller) clearTimeout(controller);
+		if(message.length < 11) 
+		{
+			response.apply(ipaddress, [true, "[error] invalid socket on message - " + message]);
+		}
+		else 
+		{
+			socket.close();
+			message = message.slice(11);
+
+			var offset = 0, object = {};
+
+			if(opcode === 'i')
+			{
+				object.passworded = !!message.readUInt8(offset);
+				object.players = message.readUInt16LE(offset += 1);
+				object.maxplayers = message.readUInt16LE(offset += 2);
+
+				object.servername = message.readUInt16LE(offset += 2);
+				object.servername = message.slice(offset += 4, offset += object.servername).toString();
+
+				object.gamemodename = message.readUInt16LE(offset);
+				object.gamemodename = message.slice(offset += 4, offset += object.gamemodename).toString();
+
+				object.language = message.readUInt16LE(offset);
+				object.language = message.slice(offset += 4, offset += object.language).toString();				
+				return response.apply(ipaddress, [ false, object ]);
+			}
+			else if(opcode === 'r') 
+			{				
+				var propertiescount = message.readUInt16LE(offset); offset += 2;
+
+				for(var i = 0; i < propertiescount; i++) 
+				{
+					var property = message.readUInt8(offset);
+					property = message.slice(++offset, offset += property).toString();
+
+					var propertyvalue = message.readUInt8(offset);
+					propertyvalue = message.slice(++offset, offset += propertyvalue).toString();
+
+					object[property] = propertyvalue;
+				}
+				return response.apply(ipaddress, [ false, object ]);
+			}
+			else if(opcode === 'd')
+			{
+				object = [];
+
+				var playerslist = [], playercount = message.readUInt16LE(offset); offset += 2;
+
+				for(var i = 0; i < playercount; i++)
+				{
+					playercount--; 
+					var player = {};
+
+					player.id = message.readUInt8(offset);
+					player.name = message.readUInt8(++offset);
+					player.name = message.slice(++offset, offset += player.name).toString();
+					player.score = message.readUInt16LE(offset);
+					player.ping = message.readUInt16LE(offset += 4);
+					offset += 4;
+
+					object.push(player);
+				}				
+				return response.apply(ipaddress, [false, object]);
+			}
+			else if(opcode === 'c')
+			{
+				object = [];
+
+				var playerslist = [], playercount = message.readUInt16LE(offset); offset += 2;
+
+				for(var i = 0; i < playercount; i++)
+				{
+					playercount--; 
+					var player = {};
+
+					player.name = message.readUInt8(offset);
+					player.name = message.slice(++offset, offset += player.name).toString();
+					player.score = message.readUInt16LE(offset);
+					offset += 4;
+
+					object.push(player);
+				}	
+				console.log(object);				
+				return response.apply(ipaddress, [false, object]);
+				
+			}
+		}
+	});
+}
+
+function validateIPAddress(ipaddress) {  
+	if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) return true;
+	return false;
+}  
+
+var getServerInfo = function(serverip, serverport, reply) {
+	safeRequest.call(this, serverip, serverport, "i", function (error, response) {
+		if(error) return reply.apply(serverip, [true, response]);
+		var serverinfo = response;
+
+		safeRequest.call(this, serverip, serverport, "r", function (error, response) {
+			if(error) return reply.apply(serverip, [true, response]);
+			serverinfo["properties"] = response;
+
+			if(parseInt(serverinfo.players) < 100)
+			{
+				safeRequest.call(this, serverip, serverport, "d", function (error, response) {
+					if(error) return reply.apply(serverip, [true, response]);
+					serverinfo["playerlist"] = response;
+
+					return reply.apply(serverip, [false, serverinfo]);
+				});
+			}
+			else return reply.apply(serverip, [false, serverinfo]);
+		});
+	});
+}
+
+var getServerProperties = function(serverip, serverport, reply) {
+	safeRequest.call(this, serverip, serverport, "r", function (error, response) {
+		if(error) return reply.apply(serverip, [true, response]);
+		return reply.apply(serverip, [false, response]);
+	});
+}
+
+var getServerOnline = function(serverip, serverport, reply) {
+	safeRequest.call(this, serverip, serverport, "i", function (error, response) {
+		if(error) return reply.apply(serverip, [true, response]);
+		return reply.apply(serverip, [false, parseInt(response.players)]);
+	});
+}
+
+var getServerMaxPlayers = function(serverip, serverport, reply) {
+	safeRequest.call(this, serverip, serverport, "i", function (error, response) {
+		if(error) return reply.apply(serverip, [true, response]);
+		return reply.apply(serverip, [false, parseInt(response.maxplayers)]);
+	});
+}
+
+var getServerName = function(serverip, serverport, reply) {
+	safeRequest.call(this, serverip, serverport, "i", function (error, response) {
+		if(error) return reply.apply(serverip, [true, response]);
+		return reply.apply(serverip, [false, response.servername]);
+	});
+}
+
+var getServerGamemodeName = function(serverip, serverport, reply) {
+	safeRequest.call(this, serverip, serverport, "i", function (error, response) {
+		if(error) return reply.apply(serverip, [true, response]);
+		return reply.apply(serverip, [false, response.gamemodename]);
+	});
+}
+
+var getServerLanguage = function(serverip, serverport, reply) {
+	safeRequest.call(this, serverip, serverport, "i", function (error, response) {
+		if(error) return reply.apply(serverip, [true, response]);
+		return reply.apply(serverip, [false, response.language]);
+	});
+}
+
+var getServerVersion = function(serverip, serverport, reply) {
+	safeRequest.call(this, serverip, serverport, "r", function (error, response) {
+		if(error) return reply.apply(serverip, [true, response]);
+		return reply.apply(serverip, [false, response.version]);
+	});
+}
+
+var getServerWeather = function(serverip, serverport, reply) {
+	safeRequest.call(this, serverip, serverport, "r", function (error, response) {
+		if(error) return reply.apply(serverip, [true, response]);
+		return reply.apply(serverip, [false, response.weather]);
+	});
+}
+
+var getServerWebSite = function(serverip, serverport, reply) {
+	safeRequest.call(this, serverip, serverport, "r", function (error, response) {
+		if(error) return reply.apply(serverip, [true, response]);
+		return reply.apply(serverip, [false, response.weburl]);
+	});
+}
+
+var getServerWorldTime = function(serverip, serverport, reply) {
+	safeRequest.call(this, serverip, serverport, "r", function (error, response) {
+		if(error) return reply.apply(serverip, [true, response]);
+		return reply.apply(serverip, [false, response.worldtime]);
+	});
+}
+
+var getServerPlayers = function(serverip, serverport, reply) {
+	getServerOnline.call(this, serverip, serverport, function (error, response) {
+		if(!isFinite(response) || response > 100) return reply.apply(serverip, [true, "[error] more 100 players on server"]);
+
+		safeRequest.call(this, serverip, serverport, "c", function (error, response) {
+			if(error) return reply.apply(serverip, [true, response]);
+			return reply.apply(serverip, [false, response]);
+		});
+	});
+}
+
+var getServerPlayersDetailed = function(serverip, serverport, reply) {
+	getServerOnline.call(this, serverip, serverport, function (error, response) {
+		if(!isFinite(response) || response > 100) return reply.apply(serverip, [true, "[error] more 100 players on server"]);
+
+		safeRequest.call(this, serverip, serverport, "d", function (error, response) {
+			if(error) return reply.apply(serverip, [true, response]);
+			return reply.apply(serverip, [false, response]);
+		});
+	});
+}
+
+var getServerPing = function(serverip, serverport, reply) {
+
+    var timenow = new Date().getTime();
+		
+	safeRequest.call(this, serverip, serverport, "i", function (error, response) {
+		if(error) return reply.apply(serverip, [true, response]);
+		return reply.apply(serverip, [false, new Date().getTime() - timenow]);
+	});
+}
+
+module.exports = getServerPing
